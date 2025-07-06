863a25bcfe559e91f2017a7502f22612583dcf03
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-06 21:10:26 +0300

    instruction added

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,16 +1,26 @@
--- Properties with average rating > 4.5
-SELECT id, name
-FROM properties
+-- 1. Users who have made more than 2 bookings
+SELECT * FROM users
+WHERE id IN (
+    SELECT user_id
+    FROM bookings
+    GROUP BY user_id
+    HAVING COUNT(*) > 2
+);
+
+-- 2. Properties with rating higher than average
+SELECT * FROM properties
 WHERE id IN (
     SELECT property_id
     FROM reviews
     GROUP BY property_id
-    HAVING AVG(rating) > 4.5
+    HAVING AVG(rating) > (
+        SELECT AVG(rating) FROM reviews
+    )
 );
 
--- Users who booked more than 3 times
-SELECT id, name
-FROM users u
-WHERE (
-    SELECT COUNT(*) FROM bookings b WHERE b.user_id = u.id
-) > 3;
+-- 3. Users who have not made any bookings
+SELECT * FROM users
+WHERE id NOT IN (
+    SELECT DISTINCT user_id FROM bookings
+    WHERE user_id IS NOT NULL
+);
